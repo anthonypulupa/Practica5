115bdf668479fc64cf3b8a566ec7d49d50efd4a7
anthonypulupa <anthonypulupa2@gmail.com>
2025-06-07 12:35:13 -0500

    Practica 5

---
+++ src/main/java/ec/edu/espoch/practica5/Practica5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practica5;
+
+/**
+ *
+ * @author anthony
+ */
+public class Practica5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
